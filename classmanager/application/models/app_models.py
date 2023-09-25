968e221b091c694d07ec2c9a13f649e924e325ea
Tobiloba Ogundiyan <114788338+6ogunt48@users.noreply.github.com>
2023-09-25 07:11:20 +0100

    compelted database models

--- classmanager/application/models/app_models.py
+++ classmanager/application/models/app_models.py
@@ -1,5 +1,6 @@
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
+from tortoise.fields import DatetimeField
 from enum import Enum
 
 
@@ -12,12 +13,14 @@ class User(models.Model):
     id = fields.IntField(pk=True)
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50)
-    username = fields.CharField(fields.CharField(max_length=50, unique=True, index=True))
+    username = fields.CharField(max_length=50, unique=True, index=True)
     email = fields.CharField(max_length=100, unique=True, index=True)
     password_hash = fields.CharField(max_length=150)
     profile_picture = fields.CharField(max_length=255, null=True)
     is_active = fields.BooleanField(default=True)
     role = fields.CharEnumField(UserRole, max_length=10)
+    created_at = DatetimeField(auto_now_add=True)  # Audit field
+    updated_at = DatetimeField(auto_now=True)  # Audit field
 
     courses = fields.ManyToManyField('models.Course', related_name='students', through='enrollments')
 
@@ -51,6 +54,8 @@ class Assignment(models.Model):
     description = fields.TextField()
     due_date = fields.DatetimeField()
     file_path = fields.CharField(max_length=255, null=True)
+    created_at = DatetimeField(auto_now_add=True)
+    updated_at = DatetimeField(auto_now=True)
 
     class Meta:
         table = "assignments"
@@ -65,9 +70,54 @@ class Marks(models.Model):
     comments = fields.TextField(null=True)
     student = fields.ForeignKeyField("models.User", related_name="marks")
     assignment = fields.ForeignKeyField("models.Assignment")
+    created_at = DatetimeField(auto_now_add=True)
+    updated_at = DatetimeField(auto_now=True)
 
     class Meta:
         table = "marks"
 
     def __str__(self):
         return f"{self.score} for {self.assignment}"
+
+
+class Notice(models.Model):
+    id = fields.IntField(pk=True)
+    course = fields.ForeignKeyField("models.Course", related_name="notices")
+    title = fields.CharField(max_length=100, index=True)
+    content = fields.TextField()
+    date_posted = fields.DatetimeField(auto_now_add=True)
+    created_at = DatetimeField(auto_now_add=True)
+    updated_at = DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "notices"
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    id = fields.IntField(pk=True)
+    sender = fields.ForeignKeyField("models.User", related_name="messages_sent")
+    receiver = fields.ForeignKeyField("models.User", related_name="messages_received")
+    content = fields.TextField()
+    date_sent = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "messages"
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver}"
+
+
+class Enrollment(models.Model):
+    student = fields.ForeignKeyField("models.User", related_name="enrollments")
+    course = fields.ForeignKeyField("models.Course")
+    date_enrolled = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "enrollments"
+        unique_together = ("student", "course")
+
+    def __str__(self):
+        return f"{self.student} enrolled in {self.course}"
